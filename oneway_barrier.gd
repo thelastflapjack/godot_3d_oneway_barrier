5472e0045c0543714b6db0e5efd5729b78f0fa7d
thelastflapjack <107695856+thelastflapjack@users.noreply.github.com>
2022-08-13 10:21:43 +0100

    Add files via upload

---
+++ oneway_barrier.gd
@@ -0,0 +1,12 @@
+extends Area
+
+
+onready var _barrier: StaticBody = $Barrier
+
+
+func _on_pass_detection_area_body_entered(body: PhysicsBody) -> void:
+	_barrier.add_collision_exception_with(body)
+
+
+func _on_pass_detection_area_body_exited(body: PhysicsBody) -> void:
+	_barrier.remove_collision_exception_with(body)
